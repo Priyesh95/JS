c2e82e5e46bbdd2c7ee434bb0d525a0007859aa6
Priyesh Nagar <priyesh.nagar1995@gmail.com>
2025-01-13 12:44:49 +0000

    Strings

---
+++ 01_basics/02_Strings.js
@@ -0,0 +1,16 @@
+let name = "Priyesh"
+
+let repoCount = 15;
+
+console.log(`My name is ${name} and my repo count is ${repoCount}`);
+
+
+let nameTwo = new String("priyesh");
+
+console.log(nameTwo.toUpperCase())
+
+console.log(nameTwo.charAt(3))
+
+console.log(nameTwo.substring(0,4))
+
+console.log(nameTwo.slice(-5,4)) // This supports negative numbers. It started from i(i is -5 considering h was -1) and ended at y(3)
