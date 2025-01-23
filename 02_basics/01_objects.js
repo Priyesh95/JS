a2aab70926620efc385667beec9427c00c7ad8de
Priyesh Nagar <priyesh.nagar1995@gmail.com>
2025-01-23 08:42:41 +0000

    Objects

--- 02_basics/01_objects.js
+++ 02_basics/01_objects.js
@@ -1,9 +1,9 @@
-const myObj = {
-    name : "Priyesh",
-    "full name" : "Priyesh Nagar",
-    email: "test@gmail.com",
-    isLoggedIn: false
-}
+// const myObj = {
+//     name : "Priyesh",
+//     "full name" : "Priyesh Nagar",
+//     email: "test@gmail.com",
+//     isLoggedIn: false
+// }
 
 // console.log(myObj["email"]);
 // console.log(myObj.email);
@@ -11,16 +11,38 @@ const myObj = {
 
 // Object.freeze(myObj);  // Now noone can change this object
 
-myObj.greeting = function(){
-    console.log(`Hello from function ${this.name}`);
-}
+// myObj.greeting = function(){
+//     console.log(`Hello from function ${this.name}`);
+// }
 
-myObj.greeting();
+// myObj.greeting();
 
 const obj1 = {a:1, b:2};
 const obj2 = {c:1, d:2};
 
-console.log(Object.assign({},obj1,obj2));
+// console.log(Object.assign({},obj1,obj2));
 
 const obj3 = {...obj1,...obj2}
-console.log(obj3);
+// console.log(obj3);
+
+
+
+// De structuring of objects
+
+const myObj = {
+    name : "Priyesh",
+    "full name" : "Priyesh Nagar",
+    email: "test@gmail.com",
+    isLoggedIn: false
+}
+
+const {email} = myObj;
+
+console.log(email);
+
+
+const {email : mail} = myObj; // Suppose if email was a longer variable so you can make is short and use it 
+
+console.log(mail);
+
+ 
